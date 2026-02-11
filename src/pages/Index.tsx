37ce8f775b626c1bc0097c13e6c267503c4af6f0
Jay-physics <gsolutionsjay@gmail.com>
2026-02-11 08:29:35 -0500

    Make credential text white for better visibility on landing page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -37,7 +37,7 @@ const Index = () => {
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <span className="text-foreground">Board Certified Psychiatrist</span>
+                <span className="text-white">Board Certified Psychiatrist</span>
               </div>
               <div className="flex items-center gap-3">
                 <div className="w-5 h-5 rounded-full bg-green-500 flex items-center justify-center">
@@ -45,7 +45,7 @@ const Index = () => {
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <span className="text-foreground">Mindfulness-Based Approaches</span>
+                <span className="text-white">Mindfulness-Based Approaches</span>
               </div>
               <div className="flex items-center gap-3">
                 <div className="w-5 h-5 rounded-full bg-green-500 flex items-center justify-center">
@@ -53,7 +53,7 @@ const Index = () => {
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <span className="text-foreground">Sports Psychiatry Focused</span>
+                <span className="text-white">Sports Psychiatry Focused</span>
               </div>
             </div>
           </div>
